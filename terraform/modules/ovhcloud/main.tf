d193f31c387a716349936b7ad2b6e766aa508cd8
Fabian Wimberger <fabian.wimberger@proton.me>
2026-03-21 11:58:11 +0100

    fix: terraform fmt on ovhcloud main.tf

--- terraform/modules/ovhcloud/main.tf
+++ terraform/modules/ovhcloud/main.tf
@@ -17,7 +17,7 @@ resource "openstack_compute_instance_v2" "benchmark" {
   flavor_name = var.instance_type
   image_id    = data.openstack_images_image_v2.os.id
   key_pair    = openstack_compute_keypair_v2.benchmark.name
-  user_data   = templatefile("${path.module}/cloud-init.yml.tmpl", {
+  user_data = templatefile("${path.module}/cloud-init.yml.tmpl", {
     ssh_public_key = var.ssh_public_key
   })
 
