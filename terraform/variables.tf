5c8426137c3cf1d8d8830cbc6e1f57a112fdbbae
Fabian Wimberger <fabian.wimberger@proton.me>
2026-03-21 11:04:22 +0100

    fix: use non-empty OVH credential fallbacks and mark project ID as sensitive

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -118,5 +118,6 @@ variable "ovh_openstack_password" {
 variable "ovh_cloud_project_id" {
   description = "OVHcloud Public Cloud project ID (service_name)"
   type        = string
+  sensitive   = true
   default     = ""
 }
